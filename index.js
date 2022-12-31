b3ae1aa93fe1a6a852d24ee43f519ae04402785c
Janet <janet_phan@outlook.com>
2022-12-31 12:45:34 -0800

    Simple JavaScript Function
    A simple javascript function with 3 parameters.

---
+++ index.js
@@ -0,0 +1,5 @@
+function intro(name, height, country) {
+    console.log('Hello my name is ' + name + '. I am ' + height + '. I come from ' + country);
+  }
+
+  intro('John', '5 feet 8 inches', 'USA');
